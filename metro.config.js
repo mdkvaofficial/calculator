a238a0df75b71f5f2843a5e1199abd92f15961a2
mdkvaofficial <mdanyalkayani.MDK@gmail.com>
2024-10-19 22:04:51 +0500

    removed extras

--- metro.config.js
+++
@@ -1,6 +0,0 @@
-const { getDefaultConfig } = require("@expo/metro-config");
-
-const defaultconfig = getDefaultConfig(__dirname);
-defaultconfig.resolver.sourceExts.push("cjs");
-
-module.exports = defaultconfig;
